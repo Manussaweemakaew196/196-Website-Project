a3bf92addcc7f7ec64ffb50a648c7f1e21eb17ad
unknown <59030196@kmitl.ac.th>
2019-05-25 21:42:55 +0700

    a

--- js/clock.js
+++ js/clock.js
@@ -1,282 +1,79 @@
-/*(function() {
-    `use strict`;
-        var canvas = document.querySelector(`#myClock`),
-        canvasContext = canvas.getContext(`2d`),
-        // clock settings
-        cX = canvas.width / 2,
-        cY = canvas.width / 2,
-        endX,
-        endY,
-        radius = 150,
-        // time settings
-        date,
-        hours,
-        minutes,
-        seconds;
-        initTime();
-        //Check for browser support
-        if (canvasContext.getContext) {
-            drawClock();
-    } else {
-        //Do something
-    }
-    setInterval(animateClock, 1000);
-    
-    // get yoour system time
-    function initTime(){
-        date = new Date();
-        hours = date.getHours() % 12;
-        minutes = date.getMinutes();
-        seconds = date.getSeconds();
-    }
-    
-    //animate the clock
-    function animateClock() {
-        clearCanvas();
-        refreshTime();
-        drawClock();
-    }
-    
-    // clear the canvas
-    function clearCanvas() {
-        canvasContext.clearRect(0, 0, canvas.width, canvas.height);
-    }
-    
-    // refresh time after 1 second
-    function refreshTime(){
-        seconds += 1;
-        if (Math.floor((seconds / 60)) != 0) { minutes += 1; seconds %= 60; }
-        if (Math.floor((minutes / 60)) != 0) { hours += 1; minutes %= 60; }
-    }
-    
-    //draw or redraw Clock after time refrexh function is called
-    function drawClock() {
-        drawClockBackground();
-        drawMinutesHand();
-        drawHoursHand();
-        drawSecondsHand();
-    }
-    
-    function drawHand(beginX, beginY, endX, endY) {
-        canvasContext.beginPath();
-        canvasContext.moveTo(beginX, beginY);
-        canvasContext.lineTo(endX, endY);
-        canvasContext.stroke();
-        canvasContext.closePath();
-    }
-    
-    // draw Hand for minutes
-    function drawMinutesHand(){
-        var rotationUnit = minutes + seconds / 60,
-            rotationFactor = Math.PI / 30,
-            rotation = rotationUnit * rotationFactor,
-            handLength = 0.9 * radius;
-        endX = cX + handLength * Math.sin(rotation);
-        endY = cY - handLength * Math.cos(rotation);
-        drawHand(cX, cY, endX, endY);
-    }
-    
-    // draw Hand for hours
-    function drawHoursHand(){
-        var rotationUnit = 5 * hours + minutes  / 12,
-            rotationFactor = Math.PI / 30,
-            rotation = rotationUnit * rotationFactor,
-            handLength = 0.6 * radius;
-        endX = cX + handLength * Math.sin(rotation);
-        endY = cY - handLength * Math.cos(rotation);
-        drawHand(cX, cY, endX, endY);
-    }
-    
-    function drawSecondsHand() {
-        var rotationUnit = seconds,
-            rotationFactor = Math.PI / 30,
-            rotation = rotationUnit * rotationFactor,
-                handLength = 1 * radius;
-        endX = cX + handLength * Math.sin(rotation);
-        endY = cY - handLength * Math.cos(rotation);
-        drawHand(cX, cY, endX, endY);
-    }
+var canvas = document.getElementById("canvas");
+var ctx = canvas.getContext("2d");
+var radius = canvas.height / 2;
+ctx.translate(radius, radius);
+radius = radius * 0.90
+setInterval(drawClock, 1000);
 
-    
-    function drawClockBackground() {
-        var correction = 1 / 300,
-            shiftUnit = 1 / 170,
-            shiftFactor = 1 / 30,
-            angleInitialPostition = 2,
-            angleCurrentPositionBegin = 0,
-            angleCurrentPositionEnd = 0,
-            repeat = 60,
-            lineWidth = 10;
-        for (var i=0; i < repeat; i+=1) {
-            angleCurrentPositionBegin = angleInitialPostition - (i * shiftFactor) - correction;
-            angleCurrentPositionEnd = angleCurrentPositionBegin + shiftUnit;
-                if(i % 5 == 0) lineWidth = 20;
-                else lineWidth = 10;
-                drawArcAtPosition(cX, cY, radius, angleCurrentPositionBegin * Math.PI, angleCurrentPositionEnd * Math.PI, false, lineWidth);
-        }
-        drawLittleCircle(cX, cY);
-    }
-    
-    function drawArcAtPosition(cX, cY, radius, startAngle, endAngle, counterClockwise, lineWidth) {
-        canvasContext.beginPath();
-        canvasContext.arc(cX, cY, radius, startAngle, endAngle, counterClockwise);
-        canvasContext.lineWidth = lineWidth;
-        canvasContext.strokeStyle = "blue";
-        canvasContext.stroke();
-        canvasContext.closePath();
-    }
-    
-    function drawLittleCircle(cX, cY) {
-        drawArcAtPosition(cX, cY, 4, 0* Math.PI, false, 4);
-    }
-    })();
-    */
-   //part1
-(function () {
-    `use strict`;
-    var canvas = document.querySelector(`#Clock`),
-        canvasContext = canvas.getContext(`2d`),
-        //clock settings
-        cX = canvas.width / 2,
-        cY = canvas.width / 2,
-        endX,
-        endY,
-        radius = 150,
-        //time settings
-        date,
-        hours,
-        minutes,
-        seconds;
-    initTime();
-    //Check for browser support
-    if (canvasContext.getContext) {
-        drawClock();
-    } else {
-        //Do something
-    }
-    setInterval(animateClock, 1000);
-    
-    //get your system time
-    function initTime() {
-        date = new Date();
-        hours = date.getHours() % 12;
-        minutes = date.getMinutes();
-        seconds = date.getSeconds();
-    }
-    
-    //part2
-    //animate the clock
-    function animateClock() {
-        clearCanvas();
-        refreshTime();
-        drawClock();
-    }
+function drawClock() {
+  drawFace(ctx, radius);
+  drawNumbers(ctx, radius);
+  drawTime(ctx, radius);
+}
 
-    //clear the canvas
-    function clearCanvas() {
-        canvasContext.clearRect(0, 0, canvas.width, canvas.height);
-    }
+function drawFace(ctx, radius) {
+  var grad;
+  ctx.beginPath();
+  ctx.arc(0, 0, radius, 0, 2*Math.PI);
+  ctx.fillStyle = 'white';
+  ctx.fill();
+  grad = ctx.createRadialGradient(0,0,radius*0.95, 0,0,radius*1.05);
+  grad.addColorStop(0, '#333');
+  grad.addColorStop(0.5, 'white');
+  grad.addColorStop(1, '#333');
+  ctx.strokeStyle = grad;
+  ctx.lineWidth = radius*0.1;
+  ctx.stroke();
+  ctx.beginPath();
+  ctx.arc(0, 0, radius*0.1, 0, 2*Math.PI);
+  ctx.fillStyle = '#333';
+  ctx.fill();
+}
 
-    //refresh time after 1 second
-    function refreshTime() {
-        seconds += 1;
-        if (Math.floor((seconds / 60)) != 0) { minutes += 1; seconds %= 60; }
-        if (Math.floor((minutes / 60)) != 0) { hours += 1; minutes %= 60; }
-    }
+function drawNumbers(ctx, radius) {
+  var ang;
+  var num;
+  ctx.font = radius*0.15 + "px arial";
+  ctx.textBaseline="middle";
+  ctx.textAlign="center";
+  for(num = 1; num < 13; num++){
+    ang = num * Math.PI / 6;
+    ctx.rotate(ang);
+    ctx.translate(0, -radius*0.85);
+    ctx.rotate(-ang);
+    ctx.fillText(num.toString(), 0, 0);
+    ctx.rotate(ang);
+    ctx.translate(0, radius*0.85);
+    ctx.rotate(-ang);
+  }
+}
 
-    //draw or redraw Clock after time refresh function is called
-    function drawClock() {
-        drawSecondHand();
-        drawMinutesHand();
-        drawHoursHand();
-        drawLittleCircle();
-        drawDefault();
-    }
+function drawTime(ctx, radius){
+    var now = new Date();
+    var hour = now.getHours();
+    var minute = now.getMinutes();
+    var second = now.getSeconds();
+    //hour
+    hour=hour%12;
+    hour=(hour*Math.PI/6)+
+    (minute*Math.PI/(6*60))+
+    (second*Math.PI/(360*60));
+    drawHand(ctx, hour, radius*0.5, radius*0.07);
+    //minute
+    minute=(minute*Math.PI/30)+(second*Math.PI/(30*60));
+    drawHand(ctx, minute, radius*0.8, radius*0.07);
+    // second
+    second=(second*Math.PI/30);
+    drawHand(ctx, second, radius*0.9, radius*0.02);
+}
 
-    //part3
-    function drawHand(beginX, beginY, endX, endY) {
-        canvasContext.beginPath();
-        canvasContext.moveTo(beginX, beginY);
-        canvasContext.lineTo(endX, endY);
-        canvasContext.stroke();
-        canvasContext.closePath();
-    }
-
-    //draw Hand for second
-    function drawSecondHand() {
-        var rotationUnit = seconds
-            rotationFactor = Math.PI / 30,
-            rotation = rotationUnit * rotationFactor,
-            handLength = 1 * radius;
-        endX = cX + handLength * Math.sin(rotation);
-        endY = cY - handLength * Math.cos(rotation);
-        drawHand(cX, cY, endX, endY);
-    }
-
-    //draw Hand for minutes
-    function drawMinutesHand() {
-        var rotationUnit = minutes + seconds / 60,
-            rotationFactor = Math.PI / 30,
-            rotation = rotationUnit * rotationFactor,
-            handLength = 0.9 * radius;
-        endX = cX + handLength * Math.sin(rotation);
-        endY = cY - handLength * Math.cos(rotation);
-        drawHand(cX, cY, endX, endY);
-    }
-
-    //draw Hand for hours
-    function drawHoursHand() {
-        var rotationUnit = 5 * hours + minutes / 12,
-            rotationFactor = Math.PI / 30,
-            rotation = rotationUnit * rotationFactor,
-            handLength = 0.6 * radius;
-        endX = cX + handLength * Math.sin(rotation);
-        endY = cY - handLength * Math.cos(rotation);
-        drawHand(cX, cY, endX, endY);
-    }
-
-    //part4
-    function drawArcAtPosition(cX, cY, radius, startAngle, endAngle, counterClockwise, lineWidth) {
-        canvasContext.beginPath();
-        canvasContext.arc(cX, cY, radius, startAngle, endAngle, counterClockwise);
-        canvasContext.lineWidth = lineWidth;
-        canvasContext.strokeStyle = "#EC407A";
-        canvasContext.stroke();
-        canvasContext.closePath();
-
-    }
-
-    //จุดวางเข็ม
-    function drawLittleCircle(cX, cY) {
-        drawArcAtPosition(cX, cY, 4, 0 * Math.PI, 2 * Math.PI, false, 4);
-    }
-
-    //รูปแบบหน้าปัด
-    function drawNumber() {
-        var ang;
-        var num;
-        canvasContext.font = radius * 0.225 + "px arial";  //ขนาดอักษร
-        canvasContext.textBaseline = "middle";
-        canvasContext.textAlign = "center";
-        canvasContext.fillStyle = 'black';  //สีอักษร
-        for (num = 1; num <= 12; num++) { 
-            ang = num * Math.PI / 6;
-            canvasContext.rotate(ang);
-            canvasContext.translate(0, -radius * 0.8);
-            canvasContext.rotate(-ang);
-            canvasContext.fillText(num.toString(), cX, cY);
-            canvasContext.rotate(ang);
-            canvasContext.translate(0, radius * 0.8);
-            canvasContext.rotate(-ang);
-        }
-    }
-    
-    function drawDefault() {
-        canvasContext.beginPath();
-        canvasContext.stroke();
-        canvasContext.strokeRect(30, 30, 340, 340);
-        canvasContext.closePath();
-        drawLittleCircle(cX, cY);
-        drawNumber();
-    }
-})();
+function drawHand(ctx, pos, length, width) {
+    ctx.beginPath();
+    ctx.lineWidth = width;
+    ctx.lineCap = "round";
+    ctx.moveTo(0,0);
+    ctx.rotate(pos);
+    ctx.lineTo(0, -length);
+    ctx.stroke();
+    ctx.rotate(-pos);
+}
